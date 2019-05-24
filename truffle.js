1145233384ac945b954398c9addb895e76e7c9e1
moseley <jeremymoseley@me.com>
2019-05-24 15:12:02 +0900

    Add .gitignore

--- truffle.js
+++ truffle.js
@@ -2,6 +2,12 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/ecf3185818414d77b45080480258aeff")
+      },
+      network_id: 3
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
